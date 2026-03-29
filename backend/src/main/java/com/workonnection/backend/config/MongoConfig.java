9b3ff2161c060ddb66d111d6506b6eeef37687cb
Guilherme <guilhermegomeszt@gmail.com>
2026-03-28 21:20:28 -0300

    criação-de-arquivo-local.env-pra-guardar-acesso-ao-atlas

--- backend/src/main/java/com/workonnection/backend/config/MongoConfig.java
+++ backend/src/main/java/com/workonnection/backend/config/MongoConfig.java
@@ -1,32 +1,23 @@
 package com.workonnection.backend.config;
 
-import com.mongodb.ConnectionString;
-import com.mongodb.MongoClientSettings;
-import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 @Configuration
 public class MongoConfig {
 
     @Bean
-    public MongoClient mongoClient() {
-        ConnectionString connectionString = new ConnectionString(
-            "mongodb+srv://admin:work123@workonnection.zx5zgsb.mongodb.net/workonnection"
-        );
+    public MongoTemplate mongoTemplate() throws IOException {
+        Properties props = new Properties();
+        props.load(new FileInputStream("local.env")); // carrega arquivo local
+        String uri = props.getProperty("MONGO_URI");
 
-        MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
-                .applyConnectionString(connectionString)
-                .retryWrites(true)
-                .build();
-
-        return MongoClients.create(mongoClientSettings);
-    }
-
-    @Bean
-    public MongoTemplate mongoTemplate() {
-        return new MongoTemplate(mongoClient(), "workonnection");
+        return new MongoTemplate(MongoClients.create(uri), "workonnection");
     }
 }
