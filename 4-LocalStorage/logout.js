379e4a972b0f536ec0c9832b795226545cc85893
Guilherme <guilhermegomeszt@gmail.com>
2026-04-21 19:33:44 -0300

    remoção-de-arquivos-antigos-de-local-storage

--- 4-LocalStorage/logout.js
+++
@@ -1,9 +0,0 @@
-// logout.js
-document.addEventListener('DOMContentLoaded', () => {
-  const btnSair = document.getElementById('btnSair');
-  if (!btnSair) return;
-  btnSair.addEventListener('click', () => {
-    localStorage.removeItem('usuarioLogado'); // chave correta
-    window.location.href = '/index.html';
-  });
-});
