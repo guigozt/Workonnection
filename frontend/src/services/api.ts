8e0eec29836e839d4be10d45ef217b61209df42b
Guilherme <guilhermegomeszt@gmail.com>
2026-08-11 09:15:58 -0300

    ajustes-de-sintaxe

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -3,4 +3,4 @@ import axios from 'axios';
 export const api = axios.create({
     baseURL: 'http://localhost:8080',
     withCredentials: true,
-})
+});
--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -1,23 +1,47 @@
-import { api } from "./api";
-import type { LoginDTO, CadastroDTO, UsuarioResponseDTO } from '../types/auth';
+import { api } from './api';
+
+import type {
+    LoginDTO,
+    CadastroDTO,
+    UsuarioResponseDTO
+} from '../types/auth';
 
 export const authService = {
+
     login: async (dadosLogin: LoginDTO) => {
-        const response = await api.post<UsuarioResponseDTO>('/usuarios/login', dadosLogin);
+
+        const response =
+            await api.post<UsuarioResponseDTO>(
+                '/usuarios/login',
+                dadosLogin
+            );
+
         return response.data;
     },
 
     cadastrar: async (dadosCadastro: CadastroDTO) => {
-        const response = await api.post<UsuarioResponseDTO>('/usuarios', dadosCadastro);
+
+        const response =
+            await api.post<UsuarioResponseDTO>(
+                '/usuarios',
+                dadosCadastro
+            );
+
         return response.data;
     },
 
     buscarUsuarioLogado: async () => {
-        const response = await api.get<UsuarioResponseDTO>('/usuarios/me');
+
+        const response =
+            await api.get<UsuarioResponseDTO>(
+                '/usuarios/me'
+            );
+
         return response.data;
     },
 
     logout: async () => {
+
         await api.post('/usuarios/logout');
     }
 };
