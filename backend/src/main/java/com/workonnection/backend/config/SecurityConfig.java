110d504c4a02a340a2398da86b6c7382b855b109
Guilherme <guilhermegomeszt@gmail.com>
2026-08-29 18:05:29 -0300

    liberação-de-url-vercel-nas-configurações-do-cors

--- backend/src/main/java/com/workonnection/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/workonnection/backend/config/SecurityConfig.java
@@ -56,7 +56,10 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of(
+            "http://localhost:5173",
+            "http://workonnection-frontend.vercel.app"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true); // Permite o envio de cookies de sessão
