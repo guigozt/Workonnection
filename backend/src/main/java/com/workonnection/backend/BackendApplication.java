f9d77fa26d7c52a9dea8eefcc84976727956cd82
Guilherme <guilhermegomeszt@gmail.com>
2026-09-10 16:56:31 -0300

    inclusão de java-dotenv para ler arquivo .env

--- backend/src/main/java/com/workonnection/backend/BackendApplication.java
+++ backend/src/main/java/com/workonnection/backend/BackendApplication.java
@@ -6,7 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        // Carrega variáveis do .env (se existir)
+        Dotenv dotenv = Dotenv.configure()
+                .directory("./")
+                .ignoreIfMissing()
+                .load();
+        dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
+        SpringApplication.run(BackendApplication.class, args);
+    }
 }
